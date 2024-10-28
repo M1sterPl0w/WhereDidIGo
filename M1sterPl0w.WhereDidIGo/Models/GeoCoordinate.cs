ca9236db0eec8c2bbc56033847e46389d5ed64a4
AzureAD\JeroenJonkmanTRES <jeroenjonkman@tres.nl>
2024-10-28 15:24:52 +0100

    feature(app): Added coordinates and some service

---
+++ M1sterPl0w.WhereDidIGo/Models/GeoCoordinate.cs
@@ -0,0 +1,9 @@
+﻿namespace M1sterPl0w.WhereDidIGo.Models
+{
+    public class GeoCoordinate
+    {
+        public required int? Longitude { get; init; }
+
+        public required int? Latitude { get; init; }
+    }
+}
---
+++ M1sterPl0w.WhereDidIGo/Services/FitService.cs
@@ -0,0 +1,25 @@
+﻿using M1sterPl0w.WhereDidIGo.Models;
+
+namespace M1sterPl0w.WhereDidIGo.Services
+{
+
+    public interface IFitService
+    {
+        Dictionary<string, List<GeoCoordinate>> DecodeZipAndWrapGeoBySport(IFormFile zipFile);
+
+        Dictionary<string, List<GeoCoordinate>> DecodeFilesAndWrapGeoBySport(List<IFormFile> files);
+    }
+
+    public sealed class FitService : IFitService
+    {
+        public Dictionary<string, List<GeoCoordinate>> DecodeFilesAndWrapGeoBySport(List<IFormFile> files)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, List<GeoCoordinate>> DecodeZipAndWrapGeoBySport(IFormFile zipFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
